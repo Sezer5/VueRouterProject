c27e85162a78a2326f2b1abe674378b26fffaf72
Sezer5 <unalmissezer@gmail.com>
2025-11-20 23:12:33 +0300

    Articles

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
     <app-header></app-header>
     <div class="container">
       <Home />
+      <Articles />
     </div>
 
 </template>
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Home from './components/Home.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import Home from './components/Home.vue';
+import Articles from './components/articles/Articles.vue';
 </script>
 <style scoped>
 
---
+++ src/components/articles/Articles.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            Deneme Articles
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
