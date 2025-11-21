e21a06bb42fedc8f9ccdcbad8bfd49208518d56d
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-11-21 17:36:31 +0300

    İletişim Sayfası Kodlandı

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <app-header></app-header>
     <div class="container">
       
-      <Article />
+      <Contact />
     </div>
 
 </template>
@@ -11,6 +11,7 @@ import { RouterLink, RouterView } from 'vue-router';
 import Home from './components/Home.vue';
 import Articles from './components/articles/Articles.vue';
 import Article from './components/articles/Article.vue';
+import Contact from './components/contact/Contact.vue';
 </script>
 <style scoped>
 
---
+++ src/components/contact/Contact.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-lg-9 col-md-8">
+            <h4 class="mb-5 text-center">İletişim</h4>
+            <form>
+                <div class="row g-3">
+                    <div class="col-sm-6">
+                        <label class="form-label">Adı:</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="col-sm-6">
+                        <label class="form-label">Soyadı:</label>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="col-12">
+                    <label class="form-label">Email:</label>
+                    <input type="email" class="form-control">
+                </div>
+                <div class="col-12">
+                    <label class="form-label">Konu:</label>
+                    <textarea class="form-control" rows="5"></textarea>
+                </div>
+                <hr class="my-4">
+                <button class="btn btn-primary btn-lg w-100">Gönder</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
