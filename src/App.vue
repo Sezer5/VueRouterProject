40067de728170358dda047a12d139ed61eddf6a4
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-11-21 17:14:51 +0300

    Tek Article Sayfası Bitişi

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <app-header></app-header>
     <div class="container">
       
-      <Articles/>
+      <Article />
     </div>
 
 </template>
@@ -10,6 +10,7 @@
 import { RouterLink, RouterView } from 'vue-router';
 import Home from './components/Home.vue';
 import Articles from './components/articles/Articles.vue';
+import Article from './components/articles/Article.vue';
 </script>
 <style scoped>
 
---
+++ src/components/articles/Article.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row bg-dark text-white p-4 rounded-5 mb-4">
+        <div class="col-md-6">
+            <h1 class="fst-italic display-4">Lorem</h1>
+            <p class="my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, officiis rem! Sequi est culpa, vitae minima minus voluptates ducimus, quidem quis ipsam earum tempora at unde vel in sit id?</p>
+        </div>
+  </div>
+  <div class="row">
+        <div class="col-md-8">
+            <h3 class="fst-italic border-bottom pb-4 mb-4">Lorem</h3>
+            <div>
+                <p>14 Haziran 2022 <strong>Sezer ÜNALMIŞ</strong></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, dolorum eveniet perspiciatis in nobis ea necessitatibus provident cupiditate recusandae minima maxime, voluptatem reiciendis sequi nesciunt libero. Rerum omnis itaque aperiam.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse facere autem necessitatibus, fugiat neque fuga nam, mollitia, reiciendis nobis voluptatem inventore. Modi quidem velit voluptatem animi nemo veniam placeat quibusdam!</p>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="bg-light p-4 rounded-5">
+                <h4 class="fst-italic">Lorem</h4>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum quas dolore illo perferendis et nobis itaque nisi consequuntur rerum eveniet laboriosam ducimus reprehenderit in, minus deserunt ullam! Fugit, exercitationem libero!</p>
+            </div>
+        </div>
+  </div>
+  
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
